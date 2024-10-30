6f82f2353bbd7b50944b0e761febd8b2f99e92f0
Sabrina0222 <ys3874@columbia.edu>
2024-10-30 05:28:36 -0400

    add methods to __init__ to be exposed

--- src/tictactoe_ys3874/__init__.py
+++ src/tictactoe_ys3874/__init__.py
@@ -1,3 +1,4 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("tictactoe_ys3874")
+from .tictactoe_ys3874 import initialize_board, make_move, check_winner, reset_game
