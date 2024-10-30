b1271491d2a18c84ccfc4948cdc3fede1792d770
Sabrina0222 <ys3874@columbia.edu>
2024-10-30 05:43:59 -0400

    version updated to 1.2.0

--- src/tictactoe_ys3874/__init__.py
+++ src/tictactoe_ys3874/__init__.py
@@ -1,4 +1,6 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("tictactoe_ys3874")
+
 from .tictactoe_ys3874 import initialize_board, make_move, check_winner, reset_game
+__all__ = ['initialize_board', 'make_move', 'check_winner', 'reset_game']
